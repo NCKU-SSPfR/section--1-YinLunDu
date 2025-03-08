698faec76919446aab6bd4ae5ebe57e016e6a819
YinLunDu <louiedu0508@gmail.com>
2025-03-08 14:24:03 +0800

    feat: dark maze

--- DarkMaze/backend/tests/test_game_over.py
+++ DarkMaze/backend/tests/test_game_over.py
@@ -1,10 +1,10 @@
 from judge_code import is_game_over
 
 def test_is_game_over_0():
-    ...
+    assert is_game_over(0) == True
 
 def test_is_game_over_666():
-    ...
+    assert is_game_over(666) == True
 
 def test_is_game_over_other():
-    ...
+    assert is_game_over(114514) == False
